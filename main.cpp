b820370951c2b2d6eb28ba7d648fb31b8c559511
s5032765 <91558903+s5032765@users.noreply.github.com>
2021-12-01 21:02:13 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+using namespace std;
 
-int main(){
-  return 0;
+bool controllo(char &c) {
+    if('A'<=c && c<='Z') {
+        c += 'a' - 'A';
+        return true;
+    }
+    else if('a'<=c && c<='z') {
+        c -= 'a' - 'A';
+        return true;
+    }
+    return false;
+}
+
+int main() {
+    char x;
+    cin>>x;
+    if(controllo(x))
+        cout<<x;
+    else
+        cout<<"errore";
+
+    return 0;
 }
